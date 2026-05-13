30e3a23468427a0fa45d10982fd5d76d26d90b96
LyonHunchoNganga <lyonnganga1@gmail.com>
2026-05-13 15:40:35 +0300

    Add initial controller imports in __init__.py

---
+++ app/controllers/__init__.py
@@ -0,0 +1,28 @@
+from .auth_controller import (
+    RegisterResource,
+    LoginResource
+)
+
+from .product_controller import (
+    ProductListResource,
+    ProductResource
+)
+
+from .order_controller import (
+    OrderListResource,
+    OrderResource
+)
+
+from .chat_controller import (
+    ChatResource
+)
+
+from .notification_controller import (
+    NotificationListResource
+)
+
+from .user_controller import (
+    UserListResource,
+    UserResource,
+    UserRoleResource
+)

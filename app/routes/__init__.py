d6821017b5f9b6f2a24ce4ec7398cb716dbd718f
Moringa School <sylvesterkonana005@gmail.com>
2026-05-19 22:26:34 +0300

    added  routes

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,7 +1,4 @@
-"""
-Routes package initializer
-Registers all API endpoints
-"""
+from flask_restful import Api
 
 from app.routes.auth_routes import register_auth_routes
 from app.routes.user_routes import register_user_routes
@@ -10,6 +7,7 @@ from app.routes.order_routes import register_order_routes
 from app.routes.order_item_routes import register_order_item_routes
 from app.routes.payment_routes import register_payment_routes
 from app.routes.review_routes import register_review_routes
+from app.routes.delivery_routes import register_delivery_routes     
 from app.routes.chat_routes import register_chat_routes
 from app.routes.notification_routes import register_notification_routes
 
@@ -24,5 +22,6 @@ def register_routes(api):
     register_order_item_routes(api)
     register_payment_routes(api)
     register_review_routes(api)
+    register_delivery_routes(api)
     register_chat_routes(api)
     register_notification_routes(api)

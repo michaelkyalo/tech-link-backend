38a04bbbf5e03439ad6305b50ee0a6d3e50146bb
Moringa School <sylvesterkonana005@gmail.com>
2026-05-20 09:33:31 +0300

    final commit

--- app/config/config.py
+++ app/config/config.py
@@ -14,9 +14,7 @@ class Config:
     )
 
     
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL"
-    )
+    SQLALCHEMY_DATABASE_URI = os.getenv( "DATABASE_URL" )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
